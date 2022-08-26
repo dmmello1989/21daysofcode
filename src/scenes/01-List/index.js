f604f6f30d9fa1a938e40fc76a37898ef4a8edd9
Daniel Mello <danielmello@aprovatotal.com.br>
2022-08-26 00:25:44 -0300

    list styling

--- src/scenes/01-List/index.js
+++ src/scenes/01-List/index.js
@@ -3,72 +3,86 @@ import * as S from "./styles";
 const list = [
   {
     number: "01",
-    text: "Repo",
+    image: "",
+    title: "Repo",
     link: "/"
   },
   {
     number: "02",
-    text: "Hello World",
+    image: "",
+    title: "Hello World",
     link: "/"
   },
   {
     number: "03",
-    text: "Animated Button",
+    image: "",
+    title: "Animated Button",
     link: "/"
   },
   {
     number: "04",
-    text: "Calculator",
+    image: "",
+    title: "Calculator",
     link: "/"
   },
   {
     number: "05",
-    text: "404 page",
+    image: "",
+    title: "404 Page",
     link: "/"
   },
   {
     number: "06",
-    text: "Glassmorphism",
+    image: "",
+    title: "Glassmorphism",
     link: "/"
   },
   {
     number: "07",
-    text: "Counter",
+    image: "",
+    title: "Counter",
     link: "/"
   },
   {
     number: "08",
-    text: "Dark/Light Mode",
+    image: "",
+    title: "Dark/Light Mode",
     link: "/"
   },
   {
     number: "09",
-    text: "Animated Loading",
+    image: "",
+    title: "Animated Loading",
     link: "/"
   },
   {
     number: "10",
-    text: "Particle.js",
+    image: "",
+    title: "Particle.js",
     link: "/"
   },
   {
     number: "11",
-    text: "Login Form",
+    image: "",
+    title: "Login Form",
     link: "/"
   },
   {
     number: "12",
-    text: "Image Gallery",
+    image: "",
+    title: "Image Gallery",
     link: "/"
   },
   {
     number: "13",
-    text: "Responsive Menu",
+    image: "",
+    title: "Responsive Menu",
     link: "/"
   },
   {
     number: "14",
-    text: "Quiz",
+    image: "",
+    title: "Quiz",
     link: "/"
   },
 ]
@@ -80,8 +94,15 @@ export const List = () => {
         <S.List>
           {list.map((item, index) => (
             <S.Item key={index}>
-              <S.Number>{item.number}</S.Number>
-              {/* <S.Image>{item.image}</S.Image> */}
+              <S.ItemInner>
+                <S.ItemFront>
+                  <S.Number>{item.number}</S.Number>
+                  
+                </S.ItemFront>
+                <S.ItemBack>
+                  <S.Image>{item.image}</S.Image>
+                </S.ItemBack>
+              </S.ItemInner>
               <S.Title>{item.title}</S.Title>
             </S.Item>
           ))}
--- src/scenes/01-List/styles.js
+++ src/scenes/01-List/styles.js
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Section = styled.section`
+  padding: 60px 0;
   background-color: var(--purple);
 `;
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 1100px;
+  max-width: 1200px;
   min-height: 100vh;
   margin: 0 auto;
 `;
@@ -20,24 +21,74 @@ export const List = styled.ul`
   align-items: center;
 `;
 
+export const ItemInner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+`;
+
+export const Title = styled.span`
+  position: absolute;
+  bottom: -64px;
+  left: 50%;
+  font-size: 2rem;
+  font-weight: 700;
+  color: var(--white);
+  transform: translateX(-50%);
+  white-space: nowrap;
+`;
+
 export const Item = styled.li`
   position: relative;
-  width: 300px;
-  height: 300px;
-  margin: 16px;
+  width: 200px;
+  height: 200px;
+  margin: 32px 32px 64px;
+  border-radius: 16px;
+  perspective: 1000px;
+  cursor: pointer;
+
+  &:hover ${ItemInner} {
+    transform: rotateY(180deg);
+  }
+
+  &:hover ${Title} {
+    bottom: -52px;
+    font-size: 2.5rem;
+    transition: all 0.4s;
+  }
+`;
+
+export const ItemFront = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 16px;
   background: rgba(0,0,0,0.3);
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden; /* Safari */
+`;
+
+export const ItemBack = styled(ItemFront)`
+  transform: rotateY(180deg);
 `;
 
 export const Number = styled.div`
   position: absolute;
-  bottom: 0;
-  right: 0;
+  bottom: -48px;
+  right: -16px;
+  width: 200px;
+  text-align: center;
   font-family: 'Russo One', sans-serif;
-  font-size: 8rem;
+  font-size: 12rem;
   color: var(--cyan);
   
 `;
 
-export const Title = styled.span`
-  
+
+export const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  background: orange;
 `;
