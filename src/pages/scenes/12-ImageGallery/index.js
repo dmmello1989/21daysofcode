4bf8ba5389a0229d5b466cf29e1e1a399b14e014
Daniel Mello <dmmello1989@gmail.com>
2022-09-28 09:41:29 -0300

    fix in column layout

--- src/pages/scenes/12-ImageGallery/index.js
+++ src/pages/scenes/12-ImageGallery/index.js
@@ -3,47 +3,16 @@ import { DetailsHeader } from "../../../components/DetailsHeader";
 import { Header } from "../../../components/Header";
 
 import Logo from "../../../images/12-image-gallery/logo-hound.webp";
-import Tree from "../../../images/12-image-gallery/tree.png";
+import Branches from "../../../images/12-image-gallery/branches.webp";
+import Garden from "../../../images/12-image-gallery/garden.webp";
+import Building from "../../../images/12-image-gallery/buildings.webp";
 import Clouds from "../../../images/12-image-gallery/clouds.png";
+import Sunglasses from "../../../images/12-image-gallery/sunglasses.webp";
 
 import * as S from "./styles";
 
-const columns = [
-  {
-    type: "image",
-    image: Tree,
-    alt: "A tree"
-  },
-  {
-    type: "image",
-    image: Clouds,
-    alt: "Some clouds"
-  },
-]
-
 export const ImageGallery = () => {
   const [isClicked, setIsClicked] = useState(false);
-
-  const IntroColumn = (src, alt) => {
-    return (
-      <S.Column>
-        <S.Logo src={Logo} alt="logo" />
-
-        <S.BottomWrapper>
-          <S.Inspired>
-            Inspired by 
-            <a href="https://dribbble.com/shots/11561969-The-First-Garment/attachments/3182669?mode=mediadirib">
-              'The First Garment'
-            </a>, by 
-            <a href="https://dribbble.com/ilochani">ilo chani</a>
-            <S.ImageWrapper>
-              <S.Image src={src} alt={alt} />
-            </S.ImageWrapper>
-          </S.Inspired>
-        </S.BottomWrapper>
-      </S.Column>
-    )
-  }
 
   return (
     <>
@@ -55,28 +24,50 @@ export const ImageGallery = () => {
       />
       <S.Section>
         <S.Container>
-          <IntroColumn src={Logo} alt="" />
-          {columns.map((column, index) => {
-            console.log(column)
-            return (
-              <S.Column key={index}>
-                {column.type === "text" && (
-                  <S.TextWrapper>
-                    <S.Strong>Malomondo Photo Gran Prix </S.Strong>
-                    <S.Divider />
-                    <S.Text>
-                      Art is breathing and retreating after shores. Nothing of the beauty goes unoticed. From the beachy waves to the highest peak, art permeates everything.
-                    </S.Text>
-                  </S.TextWrapper>
-                )}
-                {column.type === "image" && (
-                  <S.ImageWrapper>
-                    <S.Image src={column.image} alt={column.alt} />
-                  </S.ImageWrapper>
-                )}
-              </S.Column>
-            )
-          })}
+          <S.Column position="space-between">
+            <S.Logo src={Logo} alt="logo" />
+
+            <S.BottomWrapper>
+              <S.Inspired>
+                <S.Text color="#000000">
+                  Inspired by 
+                  &nbsp;<a href="https://dribbble.com/shots/11561969-The-First-Garment/attachments/3182669?mode=mediadirib">
+                    'The First Garment'
+                  </a>, by 
+                  &nbsp;<a href="https://dribbble.com/ilochani">ilo chani</a>
+                </S.Text>
+                <S.ImageWrapper>
+                  {/* <S.Image src={src} alt={alt} /> */}
+                </S.ImageWrapper>
+              </S.Inspired>
+            </S.BottomWrapper>
+          </S.Column>
+
+          <S.Column position="top">
+            <S.ImageWrapper>
+              <S.Image src={Garden} alt="A tree" />
+            </S.ImageWrapper>
+            <S.TextWrapper>
+              <S.Strong>Malomondo Photo Gran Prix </S.Strong>
+              <S.Divider />
+              <S.Text>
+                Art is breathing and retreating after shores. Nothing of the beauty goes unoticed. From the beachy waves to the highest peak, art permeates everything.
+              </S.Text>
+            </S.TextWrapper>
+          </S.Column>
+
+          <S.Column position="bottom">
+            <S.ImageWrapper>
+              <S.Image src={Sunglasses} alt="Protect you from the sun" />
+            </S.ImageWrapper>
+            <S.TextWrapper>
+              <S.Strong italic>"Protect me from the sun"</S.Strong>
+              <S.Divider />
+              <S.Text>
+                Wants pawn term, dare worsted ladle gull hoe lift wetter murder inner ladle cordage, honor itch offer lodge dock florist. Disk ladle gull orphan worry ladle
+              </S.Text>
+            </S.TextWrapper>
+          </S.Column>              
         </S.Container>
       </S.Section>
     </>
--- src/pages/scenes/12-ImageGallery/styles.js
+++ src/pages/scenes/12-ImageGallery/styles.js
@@ -7,25 +7,40 @@ export const Section = styled.div`
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: calc(100vh - 184px);
+  min-height: 100vh;
   margin: auto;
-  padding: 64px;
+  padding: 46px;
   background-color: var(--bg-calc-green);
 `;
 
 export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 100%;
-  border: 1px solid #ffffff;
-  max-width: calc(100% / 11);
+  border: 1px solid #000000;
+  max-width: calc(100% / 6);
+
+  ${props => props.position === "center" && `
+    justify-content: center;
+  `};
+
+  ${props => props.position === "bottom" && `
+    justify-content: flex-end;
+  `};
+
+${props => props.position === "space-between" && `
+    justify-content: space-between;
+  `};
 `;
 
 export const Logo = styled.img`
   max-width: 100%;
+  filter: opacity(90%);
 `;
 
 export const BottomWrapper = styled.div`
-  
+  margin-bottom: 64px;
+  padding: 16px;
 `;
 
 export const Inspired = styled.div`
@@ -37,27 +52,41 @@ export const ImageWrapper = styled.div`
 `;
 
 export const Image = styled.img`
-  filter: saturate(0%);
+  filter: grayscale(100%);
+  filter: opacity(90%);
 `;
 
 export const TextWrapper = styled.div`
   padding: 32px 8px;
+  font-family: PT Sans;
+  font-size: 0.875rem;
+  line-height: 19px;
   color: ${props => props.theme.colors.accent};
   background-color: #000000;
 `;
 
 export const Strong = styled.span`
   font-weight: 700;
-  font-size: 1.125rem;
-  font-family: var(--ff-primary);
+
+  ${props => props.italic && `
+    font-style: italic;
+  `};
 `;
 
 export const Divider = styled.div`
   height: 4px;
-  width: 30px;
-  background-color: ${props => props.theme.colors.account};
+  width: 60px;
+  margin: 16px 0;
+  background-color: ${props => props.theme.colors.accent};
 `;
 
 export const Text = styled.p`
-  
+  font-size: 0.875rem;
+  line-height: 19px;
+  color: ${props => props.color ? props.color : props.theme.colors.accent};
+
+  > a {
+    color: inherit;
+    text-decoration: underline;
+  }
 `;
